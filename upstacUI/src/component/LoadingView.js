82bcdccee5bf8aa9a5e5c9884732c6fc5b16f76b
JaishreeJaishankar <jaishree.jaishankar@riseup.net>
2021-01-17 17:46:00 +0530

    Delete upstacUI directory

--- upstacUI/src/component/LoadingView.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function LoadingView(props) {
-    return <div>Loading..</div>
-}
--- upstacUI/src/shared/loader/loading-indicator.js
+++
@@ -1,9 +0,0 @@
-import {Subject} from 'rxjs';
-
-const subject = new Subject();
-
-export const loadingIndicator = {
-    show: () => subject.next( true),
-    hide: () => subject.next(false),
-    onChange: () => subject.asObservable()
-};
